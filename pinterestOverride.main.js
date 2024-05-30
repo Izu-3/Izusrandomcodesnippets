17e9ade1d03c730b2f6e53d4409c27447a4e96de
Izu-3 <izu-3@outlook.com>
2024-05-30 05:27:20 +0100

    Update pinterestOverride.main.js

--- pinterestOverride.main.js
+++ pinterestOverride.main.js
@@ -4,20 +4,12 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       Iz
-// @match        https://www.pinterest.com*
-// @match        https://www.pinterest.co.uk*
+// @match        https://www.pinterest.com/*
+// @match        https://www.pinterest.co.uk/*
 // @require      https://code.jquery.com/jquery-3.6.0.min.js
 // @require     https://gist.github.com/raw/2625891/waitForKeyElements.js
 // @run-at      document-start
 // ==/UserScript==
-
-/*
- HERE BE DRAGONS
- THIS IS NOT HEAVILY TESTED AND SHITS ALL OVER PINTEREST'S BANDWIDTH
- YOU MAY GET 403s OR OTHER THINGS
- DO NOT USE IF YOU'RE WORRIED FOR YOUR ACCOUNT
- THIS IS NOT DESIGNED FOR REGULAR PINTEREST USE
-*/
 
 const $ = window.jQuery;
 
@@ -31,8 +23,13 @@ $(window).ready(function(){
 })
 
 function obsv(){
+// Select the node that will be observed for mutations
 const targetNode = document.getElementById('__PWS_ROOT__');
+
+// Options for the observer (which mutations to observe)
 const config = { attributes: false, childList: true, subtree: true };
+
+// Callback function to execute when mutations are observed
 const callback = (mutationList, observer) => {
   for (const mutation of mutationList) {
     if (mutation.type === 'childList') {
@@ -40,7 +37,11 @@ const callback = (mutationList, observer) => {
     }
   }
 };
+
+// Create an observer instance linked to the callback function
 const observer = new MutationObserver(callback);
+
+// Start observing the target node for configured mutations
 observer.observe(targetNode, config);
 }
 
